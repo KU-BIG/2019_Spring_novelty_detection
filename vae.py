160ca9c565f22451a261358c6e431b1c4f7f2642
Hyeonwoo Yoo <hyeon95y@gmail.com>
2019-06-04 20:08:15 +0900

    [modified] code modifed for final project experiment
    Signed-off-by: Hyeonwoo Yoo <hyeon95y@gmail.com>

--- vae.py
+++ vae.py
@@ -3,28 +3,50 @@ from torch.autograd import Variable
 
 class VAE(nn.Module) :
 
-    def __init__(self, input_dim, zdims, hdims):
+    def __init__(self, input_dim, zdims, h_num):
         super(VAE, self).__init__()
         
-        # Encoder
         self.input_dim = input_dim
-        self.fc1 = nn.Linear(input_dim, hdims)
-        
+        self.h_num = h_num
+        self.zdims = zdims
         self.leakyrelu = nn.LeakyReLU()
-        
-        self.fc21 = nn.Linear(hdims, zdims) # mu
-        self.fc22 = nn.Linear(hdims, zdims) # log_var
-        
-        
-        # Decoder
-        self.fc3 = nn.Linear(zdims, hdims)
-        
-        self.fc4 = nn.Linear(hdims, input_dim) # from latent space to output
         self.sigmoid = nn.Sigmoid()
         
+        
+        # h_num = 1
+        if self.h_num == 1 :
+            # Encoder
+            self.h1dim = round((self.input_dim+self.zdims) * (1/2))
+            self.fc1 = nn.Linear(input_dim, self.h1dim)
+            self.fc21 = nn.Linear(self.h1dim, self.zdims) # mu
+            self.fc22 = nn.Linear(self.h1dim, self.zdims) # log_var   
+            # Decoder
+            self.fc3 = nn.Linear(self.zdims, self.h1dim)
+            self.fc4 = nn.Linear(self.h1dim, self.input_dim) # from latent space to output
+            
+        # h_num = 2 :
+        if self.h_num == 2 :
+            # Encoder
+            self.h1dim = round((self.input_dim+self.zdims) * (2/3))
+            self.h2dim = round((self.input_dim+self.zdims) * (1/3))
+            self.fc1 = nn.Linear(input_dim, self.h1dim)
+            self.fc2 = nn.Linear(self.h1dim, self.h2dim)
+            self.fc31 = nn.Linear(self.h2dim, self.zdims)
+            self.fc32 = nn.Linear(self.h2dim, self.zdims)
+            # Decoder
+            self.fc4 = nn.Linear(self.zdims, self.h2dim)
+            self.fc5 = nn.Linear(self.h2dim, self.h1dim)
+            self.fc6 = nn.Linear(self.h1dim, self.input_dim)
+        
+        
     def encode(self, x) :
-        h1 = self.leakyrelu(self.fc1(x))
-        return self.fc21(h1), self.fc22(h1) # returns mu, log_var
+        if self.h_num == 1 :
+            h1 = self.leakyrelu(self.fc1(x))
+            return self.fc21(h1), self.fc22(h1) # returns mu, log_var
+        if self.h_num == 2 :
+            h1 = self.leakyrelu(self.fc1(x))
+            h2 = self.leakyrelu(self.fc2(h1))
+            return self.fc31(h2), self.fc32(h2)
     
     def reparametrize(self, mu, logvar) : 
         if self.training :
@@ -38,8 +60,13 @@ class VAE(nn.Module) :
             return mu
         
     def decode(self, z) :
-        h3 = self.leakyrelu(self.fc3(z))
-        return self.sigmoid(self.fc4(h3)) # final output, reconstruction of mnist image
+        if self.h_num == 1 :
+            h3 = self.leakyrelu(self.fc3(z))
+            return self.sigmoid(self.fc4(h3)) # final output, reconstruction of mnist image
+        if self.h_num == 2 :
+            h4 = self.leakyrelu(self.fc4(z))
+            h5 = self.leakyrelu(self.fc5(h4))
+            return self.sigmoid(self.fc6(h5))
     
     def forward(self, x) :
         mu, logvar = self.encode(x.view(-1, self.input_dim))
